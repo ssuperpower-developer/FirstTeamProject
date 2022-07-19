641ce53671f7279b28afcfe047336c2ba3045330
Jiwoong Choi <jwchoi179@soongsil.ac.kr>
2022-07-19 23:40:03 +0900

    [Docs] 협업 작업 이전 기본 세팅

---
+++ Account.h
@@ -0,0 +1,37 @@
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+class Account {
+private:
+  int accountID;
+  int balance;
+
+public:
+  Account(int ID, int money, char *name);
+  Account(const Account &ref);
+
+  int GetAccID() const;
+  void Deposit(int money);
+  int Withdraw(int money);
+  void ShowAccInfo() const;
+  ~Account();
+};
+
+class AccountHandler {
+private:
+  Account *accArr[100];
+  int accNum;
+
+public:
+  AccountHandler();
+  void ShowMenu(void) const;
+  void MakeAccount(void);
+  void DepositMoney(void);
+  void WithdrawMoney(void);
+  void ShowAllAccInfo(void) const;
+  ~AccountHandler();
+};
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# SSUPERPOWER DEVELOPER First Team Project
+# Git Practice Project
+
+## How to write Commit Message
+
+1. 파일 추가 : Add
+2. 버그 수정 : Fix
+3. 리팩터링 : Refactoring
+4. 주석 추가 : Comment
+5. 파일 삭제 : Remove
+6. 기능 추가 : Feat
+
+Ex) [Feat] Len 함수 기능 완성
