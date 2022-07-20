dac5031ef03fc1acdacb220c5f548126cb753177
rover0811 <rover0811@hotmail.com>
2022-07-20 17:58:11 +0900

    [Refactoring] namePtr -> cusName 변수명 수정

--- Account.cpp
+++ Account.cpp
@@ -2,12 +2,12 @@
 #include <cstring>
 #include "Account.h"
 Account::Account(const Account &ref):accountID(ref.accountID),balance(ref.balance) {//복사 생성자
-    namePtr=new char [strlen(ref.namePtr)+1]; //널문자 자리 하나
-    strcpy(namePtr,ref.namePtr);// 깊은 복사
+    cusName=new char [strlen(ref.cusName)+1]; //널문자 자리 하나
+    strcpy(cusName,ref.cusName);// 깊은 복사
 }
 Account::Account(int ID, int money, char *name): accountID(ID),balance(money) {// 생성자
-    namePtr=new char [strlen(name)+1]; //널문자 자리 하나
-    strcpy(namePtr,name);// 깊은 복사
+    cusName=new char [strlen(name)+1]; //널문자 자리 하나
+    strcpy(cusName,name);// 깊은 복사
 }
 
 int Account::GetAccID() const {
