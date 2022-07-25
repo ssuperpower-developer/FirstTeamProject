d191bd66dc9f11e337f5729b0d27ceaf8dd4f0f7
Hongjunehuke <junhyeok2166@daum.net>
2022-07-25 16:51:06 +0900

    [Feat] AccountHandler_DepositMoney

--- Accounthandler.cpp
+++ Accounthandler.cpp
@@ -15,7 +15,7 @@ void AccountHandler::WithdrawMoney(void)
 
     for (int i = 0; i < accNum; i++)
     {
-        if (accArr[i]->GetAccID == id)
+        if (accArr[i]->GetAccID() == id)
         {
             if (accArr[i]->Withdraw(money) < 0)
             {
@@ -26,5 +26,25 @@ void AccountHandler::WithdrawMoney(void)
         cout << "출금 완료" << endl;
     }
     cout << "유효하지 않은 ID 입니다" << endl;
+    return;
+}
+
+void AccountHandler::DepositMoney(void){
+    int money;
+    int id;
+    cout<<"[입   금]"<<endl;
+    cout<<"계좌번호 : ";
+    cin>>id;
+    cout<<"입금액 : ";
+    cin>>money;
+    for(int i=0;i<accNum;i++){
+        if(accArr[i]->GetAccID() == id){
+            accArr[i]->Deposit(money);
+            cout<<"입금 완료"<<endl;
+            return;
+        }
+        else    continue;
+    }
+    cout<<"없는 계좌번호입니다."<<endl;
     return;
 }
